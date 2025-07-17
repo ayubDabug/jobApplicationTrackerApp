621aff3356b6359a35910ea7b97da6a427b64d5f
Ayub Abdelgawad <ayubabdelgawad@gmail.com>
2025-07-17 16:59:53 -0400

    new things!

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
 app.listen(HTTP_PORT, () => console.log(`server listening on ${HTTP_PORT}`));
